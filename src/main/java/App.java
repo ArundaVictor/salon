4b6ed13625d9725141e4c0f7787282f052a0c6cf
Arunda Victor <victoroyugi31@gmail.com>
2019-02-07 17:34:22 +0300

    update stylist class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -104,6 +104,27 @@ public class App {
   return new ModelAndView(model, layout);
 }, new VelocityTemplateEngine());
 
+//            post("/stylists/:stylist_id", (request, response) -> {
+//   Map<String, Object> model = new HashMap<String, Object>();
+//   Stylist stylist = Stylist.find(Integer.parseInt(request.params("id")));
+//   String name = request.queryParams("name");
+//   Stylist stylist = Stylist.find(stylist.getStylistId());
+//   stylist.update(name);
+//   String url = String.format("/stylists/%d/clients/%d", stylist.getId());
+//   response.redirect(url);
+//   return new ModelAndView(model, layout);
+// }, new VelocityTemplateEngine());
+
+//           post("/stylists/:stylist_id/clients/:id/delete", (request, response) -> {
+//   HashMap<String, Object> model = new HashMap<String, Object>();
+//   Client client = Client.find(Integer.parseInt(request.params("id")));
+//   Stylist stylist = Stylist.find(client.getStylistId());
+//   client.delete();
+//   model.put("stylist", stylist);
+//   model.put("template", "templates/stylist.vtl");
+//   return new ModelAndView(model, layout);
+// }, new VelocityTemplateEngine());
+
 
 
    
